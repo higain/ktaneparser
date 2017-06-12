87e02f3ea6e5a7e44e4d71d7b4c284e82fa62548
Maxi Stauss <maxi.stauss@t-online.de>
2017-06-12 19:52:03 +0200

    Eigene Missionen können gestartet werden Missionsstart stabiler geschrieben (Failsafe) Start und Stop von Prozess verbessert Logfile-Code auskommentiert

--- src/KtaneHandler.java
+++ src/KtaneHandler.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -20,9 +22,17 @@ public class KtaneHandler {
     private String ktaneStarter = "D:/workspace/IISM/ktane_start.vbs";
     private String ktaneExeLocation = "C:/Program Files (x86)/Steam/steamapps/common/Keep Talking and Nobody Explodes/ktane.exe";
     private String[] ktaneFromSteam = {"C:/Program Files (x86)/Steam/Steam.exe", "-applaunch", "341800"};
-    private String[] missionIds = {"firsttime", "mission", "mission 1","mission 2"};
+    private String[] missionIds1 = {"mod_iismkitpretest1_1bomb1", "mod_iismkitpretest1_1bomb2", "mod_iismkitpretest1_1bomb3",
+            "mod_iismkitpretest1_1bomb4", "mod_iismkitpretest1_1bomb5", "mod_iismkitpretest1_1bomb6",
+            "mod_iismkitpretest1_1bomb7", "mod_iismkitpretest1_1bomb8", "mod_iismkitpretest1_1bomb9",
+            "mod_iismkitpretest1_1bomb10"};
+    private String[] missionIds2 = {"mod_iismkitpretest2_2bomb1", "mod_iismkitpretest2_2bomb2", "mod_iismkitpretest2_2bomb3",
+            "mod_iismkitpretest2_2bomb4", "mod_iismkitpretest2_2bomb5", "mod_iismkitpretest2_2bomb6",
+            "mod_iismkitpretest2_2bomb7", "mod_iismkitpretest2_2bomb8", "mod_iismkitpretest2_2bomb9",
+            "mod_iismkitpretest2_2bomb10"};
+    private String[] seeds = {"11779", "13718", "49060", "59085", "27392", "1120", "59549", "44791", "643", "41167"};
 
-    private Process runningGame;
+    private String runningGame;
     private boolean run;
     public boolean experiment;
     private int numberOfRounds = 0;
@@ -32,7 +42,6 @@ public class KtaneHandler {
     private String timeLeft, bombState;
     private long strikes;
 
-    private String[] bombList = {"abc", "def"};
 
     KtaneJsonHandler ktjshandler;
 
@@ -40,35 +49,42 @@ public class KtaneHandler {
     }
 
     public void go(int numberOfGames) {
-        initialize();
         System.out.println("Aktuellster TS: " + latestTs);
 
-        while(numberOfGames>=numberOfRounds) {
+        while (numberOfGames > numberOfRounds) {
+            initialize();
             System.out.println("Starting Game from " + Arrays.toString(ktaneFromSteam));
             runningGame = startGame(ktaneFromSteam);
-            ShutdownThread shutdownHandler = new ShutdownThread(runningGame);
-            Runtime.getRuntime().addShutdownHook(shutdownHandler);
-            schlafen(20000);
-            startMission(missionIds[0]);
-            schlafen(10000);
-            run = true;
+            schlafen(15000);
+            while (!startMission(numberOfRounds)) {
+                schlafen(200);
+                System.out.println("Konnte mission nicht starten.");
+            }
+            // startMission(numberOfRounds);
+            schlafen(5000);
             numberOfRounds++;
-            System.out.println("Start game Nr. " + numberOfRounds);
-            latestTs = new Timestamp(new Date().getTime());
-
-            while (run) {
-                // System.out.println(ktjshandler.fetchBombInfos());
+            System.out.println("Start game Nr. " + numberOfRounds + " of " + numberOfGames);
+            // used for log file parsing latestTs = new Timestamp(new Date().getTime());
+            while (bombState == null) {
                 parseJson(ktjshandler.fetchBombInfos());
-
-                if (processFile()) {
-                    System.out.println("Spiel zu ende");
-                    schlafen(1000);
-                    run = false;
-                    runningGame.destroy();
-                }
+                schlafen(200);
+                System.out.println("Mission noch nicht fertig geladen.");
+            }
+            while (!(bombState.equals("Exploded"))) {
+                parseJson(ktjshandler.fetchBombInfos());
                 schlafen(500);
             }
+            System.out.println("Spiel zu ende");
+            bombState = null;
+            System.out.println("Waiting for Process: " + runningGame);
+            schlafen(3000);
+            stopGame(runningGame);
+            System.out.println("Shutting down game");
+            schlafen(7000);
+            strikes = 0;
+            timeLeft = bombState = runningGame = solvableModules =  modules =  solvedModules = null;
         }
+        System.out.println("Experiment zu ende!");
     }
 
     public void parseJson(JSONObject jobj) {
@@ -81,22 +97,16 @@ public class KtaneHandler {
             bombState = (String) jobj.get("BombState");
 
             printBombStatus();
-        }
-        catch (NullPointerException npe) {
+        } catch (NullPointerException npe) {
             System.out.println("Cannot parse Json Object, is the game running?");
             System.out.println(npe.getLocalizedMessage());
         }
     }
 
     public void printBombStatus() {
-        System.out.println("Solvable Modules: "+solvableModules);
-        System.out.println("Solved Modules: "+solvedModules);
-        System.out.println("Modules to solve: " + modules);
-        System.out.println("Time left: "+timeLeft);
-        System.out.println("Strikes: "+strikes);
-        System.out.println("State: "+bombState);
-        System.out.println();
-
+        System.out.println("Solvable Modules: " + solvableModules + " " + "Solved Modules: " + solvedModules + " " +
+                "Modules to solve: " + modules + " " + "Time left: " + timeLeft + " " + "Strikes: " + strikes +
+                " " + "State: " + bombState);
     }
 
     public boolean schlafen(long mil) {
@@ -110,30 +120,75 @@ public class KtaneHandler {
     }
 
     public void initialize() {
-        System.out.println("Reading log file: " + logFile);
+        // System.out.println("Reading log file: " + logFile);
         ktjshandler = new KtaneJsonHandler(8085, "http://localhost:8085/");
     }
 
+    public boolean stopGame(String pid) {
+        String cmd = "taskkill /F /PID " + pid;
+        try {
+            Runtime.getRuntime().exec(cmd);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-    public Process startGame(String[] ktaneLocation) {
+
+    public String startGame(String[] ktaneLocation) {
         try {
             // Runtime.getRuntime().exec(ktaneLocation);
             Process ktaneProcess = new ProcessBuilder(ktaneLocation).start();
+            System.out.println("Process started");
+            boolean started = false;
+            String pid = "";
+            while (!started) {
+                try {
+                    String line;
+                    Process p = Runtime.getRuntime().exec
+                            (System.getenv("windir") + "\\system32\\" + "tasklist.exe");
+                    BufferedReader input =
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    while ((line = input.readLine()) != null) {
+                        if (line.contains("ktane.exe")) {
+                            String[] tmparray = line.split(" ");
+                            System.out.println(Arrays.toString(tmparray));
+                            ArrayList<String> regline = new ArrayList<>();
+                            for (String s : tmparray) {
+                                if (s.length() > 1) {
+                                    regline.add(s.replace(" ", ""));
+                                }
+                            }
+                            System.out.println("Anzahl Elemente: " + regline.size());
+                            System.out.println(Arrays.toString(regline.toArray()));
+                            if (regline.size() > 2) {
+                                System.out.println(regline.get(1));
+                                pid = regline.get(1);
+                            }
+                            started = true;
+                            break;
+                        }
+                    }
+                    input.close();
+                } catch (Exception err) {
+                    err.printStackTrace();
+                }
+            }
 
-            return ktaneProcess;
+            return pid;
         } catch (IOException e) {
             e.printStackTrace();
+            schlafen(200);
+            startGame(ktaneLocation);
             return null;
         }
     }
 
-    public void startMission(String missionId) {
-        int randomNum = ThreadLocalRandom.current().nextInt(1234, 99998 + 1);
-        boolean state = ktjshandler.startMission(missionId, ""+randomNum);
-        System.out.println(state);
+    public boolean startMission(int missionId) {
+        boolean state = ktjshandler.startMission(missionIds1[missionId], "" + seeds[missionId]);
+        return state;
     }
-
-
 
 
     /**
